3df1f06621f1c30f5e0d30d5c33c9ad8f8532fe6
AntonyShaps <16188347+AntonyShaps@users.noreply.github.com>
2024-01-09 18:58:27 +0100

    4.8-4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -8,20 +8,46 @@ blogsRouter.get('/', async(request, response) => {
   response.json(blogs)
   })
 
-blogsRouter.get('/:id', (request, response) => {
-  Blog.findById(request.params.id).then(blog=> {
-    if (blog) {
-        response.json(blog)
-      }else{
-        response.status(404).end()
-      }
-  })
+blogsRouter.get('/:id', async(request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
 })
 
-blogsRouter.post('/', async(request, response) => {
-    const blog = new Blog(request.body)
-    const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
+blogsRouter.post('/', async (request, response) => {
+
+  if (!request.body.title || !request.body.url) {
+    return response.status(400).send('Title and URL are required.')
+  }
+
+  if (typeof request.body.likes !== 'number') {
+    request.body.likes = 0
+  }
+
+  const blog = new Blog(request.body)
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+blogsRouter.put('/:id', async (request, response) => {
+	const body = request.body
+  
+	const blog = {
+		title: body.title,
+		author: body.author,
+		url: body.url,
+		likes: body.likes ? body.likes : 0 ,
+	}
+  
+	await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+	response.json(blog)
   })
 
 
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -3,31 +3,11 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
-
-
-const initialBlogs =[
-    {
-        title: "Alfred is the best",
-        author: "Herr Kozhitz",
-        url: "nourl",
-        likes: 0,
-        id: "659ae23cd2f43196cf80c3e9"
-    },
-    {
-        title: "probability of being born in Africa",
-        author: "Jumanju Jumanjovic",
-        url: "losturl",
-        likes: 0,
-        id: "659ae26fd2f43196cf80c3eb"
-    }
-]
+const helper = require('./test_helper')
 
 beforeEach(async () => {
-    await Blog.deleteMany({})
-    let blogObject = new Blog(initialBlogs[0])
-    await blogObject.save()
-    blogObject = new Blog(initialBlogs[1])
-    await blogObject.save()
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
 })
 
 test('blogs are returned as json', async () => {
@@ -37,20 +17,51 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+test('id defined', async () => {
+  const response = await helper.nonExistingId()
+  expect(response).toBeDefined()
+})
+
+test('if likes are undefined, default to 0', async () => {
+  const newBlog = {
+    title: 'async/await simplifies making async calls',
+    author: 'matti',
+    url: 'fullstack',
+    likes:''
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get(`/api/blogs/`)
+  expect(response.body.slice(-1)[0].likes).toEqual(0)
+})
+test('if title or url are undefined, 400', async () => {
+  const newBlog = {
+    title: "",
+    author: 'matti',
+    url: "",
+    likes:''
+  }
+
+  await api
+    .post('/api/blogs')
+    .expect(400)
+})
+
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
-
-
-  expect(response.body).toHaveLength(initialBlogs.length)
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
 test('a specific blog is within the returned blogs', async () => {
   const response = await api.get('/api/blogs')
-
-
   const contents = response.body.map(r => r.url)
   expect(contents).toContain(
-    'losturl'
+    'https://reactpatterns.com/'
   )
 })
 test('a valid blog can be added', async () => {
@@ -58,7 +69,7 @@ test('a valid blog can be added', async () => {
       title: 'async/await simplifies making async calls',
       author: 'matti',
       url: 'fullstack',
-      likes: 0
+      likes: 1,
     }
   
     await api
@@ -71,11 +82,53 @@ test('a valid blog can be added', async () => {
   
     const contents = response.body.map(r => r.title)
   
-    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
     expect(contents).toContain(
       'async/await simplifies making async calls'
     )
 })
+
+describe('deletion of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(
+      helper.initialBlogs.length - 1
+    )
+
+    const contents = blogsAtEnd.map(r => r.id)
+
+    expect(contents).not.toContain(blogToDelete.id)
+  })
+})
+
+describe('update of a blog', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
+    const updatedBlog = {
+      ...blogToUpdate,
+      likes:5
+    }
+
+    await api
+      .put(`/api/blogs/${updatedBlog.id}`)
+      .send(updatedBlog)
+      .expect(200)
+
+    const blogsAtEnd = await api.get(`/api/blogs/${updatedBlog.id}`)
+
+    expect(blogsAtEnd.body.likes).toEqual(5)
+  })
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
